d409998632c06b8cf778780bf1313007706bcd20
prandeep28 <prandeep28@gmail.com>
2024-02-24 22:50:42 +0530

    player correct

--- js/index.js
+++ js/index.js
@@ -11,9 +11,9 @@ var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", randomImageSource2);
 
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
 } else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = "🚩 Play 2 Wins!";
+    document.querySelector("h1").innerHTML = "🚩 Player 2 Wins!";
 } else {
     document.querySelector("h1").innerHTML = "Draw!";
 }
@@ -33,9 +33,9 @@ var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", randomImageSource2);
 
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
 } else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = "🚩 Play 2 Wins!";
+    document.querySelector("h1").innerHTML = "🚩 Player 2 Wins!";
 } else {
     document.querySelector("h1").innerHTML = "Draw!";
 }
